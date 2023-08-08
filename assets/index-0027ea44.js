04ffdbb2485aba385b4e2e60eb4c14d720c231fb
bf_tsai <tsai19911026@gmail.com>
2023-08-08 17:01:40 +0800

    Updates

---
+++ assets/index-0027ea44.js
@@ -0,0 +1,1 @@
+import"./main-ee9675f0.js";$(document).ready(function(){$(".reply").click(function(e){e.preventDefault(),$(".bottom").toggleClass("hidden"),$(".own").toggleClass("hidden"),$(".replyForm").toggleClass("show")}),$(".suggestReply").click(function(e){e.preventDefault(),$(".bottom").toggleClass("hidden"),$(".own").toggleClass("hidden"),$(".replyForm").toggleClass("show")})});
--- assets/index-95b717d6.js
+++
@@ -1,1 +0,0 @@
-import"./main-816c499c.js";$(document).ready(function(){$(".reply").click(function(t){t.preventDefault(),$(".bottom").toggleClass(".hidden")})});
