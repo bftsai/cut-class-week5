cc436985794c7800cb6b111e24e8f67833b2bf4b
bf_tsai <tsai19911026@gmail.com>
2023-08-08 17:01:20 +0800

    index.js building

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,6 +1,14 @@
 $(document).ready(function () {
     $('.reply').click(function (e) { 
         e.preventDefault();
-        $('.bottom').toggleClass('.hidden');
+        $('.bottom').toggleClass('hidden');
+        $('.own').toggleClass('hidden');
+        $('.replyForm').toggleClass('show');
+    });
+    $('.suggestReply').click(function (e) { 
+        e.preventDefault();
+        $('.bottom').toggleClass('hidden');
+        $('.own').toggleClass('hidden');
+        $('.replyForm').toggleClass('show');
     });
 });
