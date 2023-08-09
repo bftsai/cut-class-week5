2e74e3e08d1ea0299bc0ccbaac264ef63a993192
bf_tsai <tsai19911026@gmail.com>
2023-08-09 16:28:57 +0800

    first finished

--- assets/js/adimn.js
+++ assets/js/adimn.js
@@ -1,3 +1,7 @@
 $(document).ready(function () {
     $('.nav-link5').addClass('focus');
+    $('.verified1').click(function (e) { 
+        e.preventDefault();
+        $('.item1Deg0').toggleClass('deg180');
+    });
 });
